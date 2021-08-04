65015514948e191fa096a48a9f80b4c9d4d3f4e3
Tsega <tsega207@gmail.com>
2021-08-04 23:34:55 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// ReactDOM.render(
+//   <Component1 />,
+//   document.getElementById('imgContainer')
+// );
