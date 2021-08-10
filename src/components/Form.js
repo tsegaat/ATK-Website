9e292f8a2513012d06f0f69b7a8ee1f87a4e1941
Tsega <tsega207@gmail.com>
2021-08-10 22:33:53 +0300

    commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,29 @@
 import React from 'react';
 
+
 const Form = () => {
+    const nameRef = React.createRef();
+    const emailRef = React.createRef();
+    const subjectRef = React.createRef();
+    const commentRef = React.createRef();
+
+    const sendMessageHandler = (e) => {
+        e.preventDefault();
+        const name = nameRef.current.value
+        const email = emailRef.current.value
+        const subject = subjectRef.current.value
+        const comment = commentRef.current.value
+
+        // Do some stuff and let the user send you an email
+        console.log(nameRef.current.value)
+    }
+
     return(
-            <form action="/action_page.php" target="_blank">
-                <input className="w3-input w3-border" type="text" placeholder="Name" required name="Name"/>
-                <input className="w3-input w3-section w3-border" type="text" placeholder="Email" required name="Email"/>
-                <input className="w3-input w3-section w3-border" type="text" placeholder="Subject" required name="Subject"/>
-                <input className="w3-input w3-section w3-border" type="text" placeholder="Comment" required name="Comment"/>
+            <form onSubmit={sendMessageHandler} target="_blank">
+                <input className="w3-input w3-border" type="text" placeholder="Name" required name="Name" ref={nameRef}/>
+                <input className="w3-input w3-section w3-border" type="text" placeholder="Email" required name="Email" ref={emailRef}/>
+                <input className="w3-input w3-section w3-border" type="text" placeholder="Subject" required name="Subject" ref={subjectRef}/>
+                <input className="w3-input w3-section w3-border" type="text" placeholder="Comment" required name="Comment" ref={commentRef}/>
                 <button className="w3-button w3-black w3-section" type="submit">
                     <i className="fa fa-paper-plane"></i> SEND MESSAGE
                 </button>
