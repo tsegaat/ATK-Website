f3c1e4feafc881707da45b04c6ef7c3415e5d4f2
Tsega <tsega207@gmail.com>
2021-08-11 10:14:29 +0300

    comi

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -31,49 +31,7 @@ const Footer = () => {
         <section className="">
           <div className="container text-center text-md-start mt-5">
             <div className="row mt-3">
-              <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">
-                  <i className="fas fa-gem me-3"></i>ATK
-                </h6>
-                <p>
-                  Here you can use rows and columns to organize your footer content. Lorem ipsum
-                  dolor sit amet, consectetur adipisicing elit.
-                </p>
-              </div>
-              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">
-                  Products
-                </h6>
-                <p>
-                  <a href="#!" className="text-reset">Angular</a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">React</a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">Vue</a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">Laravel</a>
-                </p>
-              </div>
-              <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">
-                </h6>
-                <p>
-                  <a href="#!" className="text-reset">Pricing</a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">Settings</a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">Orders</a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset">Help</a>
-                </p>
-              </div>
-              <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+              <div className="col-md-4 col-lg-3 col-xl-3 mb-md-0 mb-4" style={{"marginLeft": "auto"}}>
 
                 <h6 className="text-uppercase fw-bold mb-4">
                   Contact
