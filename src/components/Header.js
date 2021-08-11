c038939b888f615a21acb4940152e5990925c0f6
Tsega <tsega207@gmail.com>
2021-08-11 10:48:51 +0300

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="w3-display-container w3-content w3-wide" style={{"maxWidth": "1500px"}} id="home">
         <img className="w3-image" src="https://via.placeholder.com/1500x800.png?text=_" alt="Architecture" width="1500" height="800"/>
         <div className="w3-display-middle w3-margin-top w3-center">
-          {/* <img className="header-img" src={bannerLogo} alt="Atk Banner" /> */}
+          <img className="header-img" src={bannerLogo} alt="Atk Banner" />
         </div>
     </header>
     )
