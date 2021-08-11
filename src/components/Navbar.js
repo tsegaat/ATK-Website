2f9080c02f481def1d0746cb01d960bfb9428a09
Tsega <tsega207@gmail.com>
2021-08-11 11:07:20 +0300

    Adding service to the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,10 @@ const Navbar = () => {
     <div className="w3-top">
         <div className="w3-bar w3-white w3-wide w3-padding w3-card">
             <a href="#home" className="w3-bar-item"><img src={logo} alt="ATK" style={{ "height": "50px"}}/></a>
-            <div className="w3-right w3-hide-small nav-options-container">
-                <a href="#projects" className="w3-bar-item">Projects</a>
+            <div className="w3-right w3-hide-small nav-options-container">                
                 <a href="#about" className="w3-bar-item">About</a>
+                <a href="#serviceContainer" className="w3-bar-item">Services</a>
+                <a href="#projects" className="w3-bar-item">Projects</a>
                 <a href="#contact" className="w3-bar-item">Contact</a>
             </div>
         </div>
