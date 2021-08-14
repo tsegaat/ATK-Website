3831be25a2dc050c026b37046d0e7422db31543b
Tsega <tsega207@gmail.com>
2021-08-14 23:29:54 +0300

    updates

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
             <div className="contact-map-col-2">
                 <div className="w3-container w3-padding-32">
                     <h3 className="w3-border-bottom w3-border-light-grey w3-padding-16">Contact</h3>
-                    <p>Lets get in touch and talk about your next project.</p>
+                    <p>Lets get in touch and talk about our next project.</p>
                     <Form />
                 </div>
                 <div>
