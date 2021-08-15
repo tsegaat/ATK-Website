2be259433e6a782bd3a8a88c6e59f87d6467e0cd
Tsega <tsega207@gmail.com>
2021-08-15 23:54:22 +0300

    Project on hover small description

--- atk-fe/src/components/Project.js
+++ atk-fe/src/components/Project.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Employees = ({name, img}) => {
     return(
-        <div className="w3-display-container">
-            <div className="w3-display-topleft w3-black w3-padding">{name}</div>
-            <img src={img} alt="House" style={{width: "100%"}}/>
+        <div className="w3-display-container w3-darken-img">       
+            <div className="w3-display-topleft w3-black w3-padding project-name">{name}</div>
+            <div className="w3-display-middle project-description">Phasellus eget enim eu lectus faucibus vestibulum.</div>
+            <div className="dark-bg"></div>
+            <img src={img} alt="House" style={{"width": "100%"}}/>
+
         </div>
     )
 }
