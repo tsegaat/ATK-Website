a0f2ad098409b99c423146b37c1eb6c9c4896a55
Tsega <tsega207@gmail.com>
2021-08-15 22:01:06 +0300

    NavBar items color set

--- atk-fe/src/components/Navbar.js
+++ atk-fe/src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div className="w3-top">
         <div className="w3-bar w3-white w3-wide w3-padding w3-card">
             <a href="#home" className="w3-bar-item"><img src={logo} alt="ATK" style={{ "height": "50px"}}/></a>
-            <div className="w3-right w3-hide-small nav-options-container">                
+            <div className="w3-right w3-hide-small nav-options-container" style={{ "color": "inherit !important"}}>                
                 <a href="#about" className="w3-bar-item">About</a>
                 <a href="#service" className="w3-bar-item">Services</a>
                 <a href="#projects" className="w3-bar-item">Projects</a>
