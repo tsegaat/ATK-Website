6cf5644fb963b13c457ffd1ea4316c78d0f04389
Tsega <tsega207@gmail.com>
2021-08-16 11:16:57 +0300

    Final form reset commit

--- atk-fe/src/components/Form.js
+++ atk-fe/src/components/Form.js
@@ -2,10 +2,6 @@ import React from 'react';
 
 
 const Form = () => {
-    const nameRef = React.createRef();
-    const emailRef = React.createRef();
-    const subjectRef = React.createRef();
-    const commentRef = React.createRef();
     const clearInputs = () => {
         setTimeout(() => {
             const form = document.querySelector("form")
@@ -15,10 +11,10 @@ const Form = () => {
     }
     return(
             <form action="/contact" onSubmit={clearInputs} target="_blank" method="POST">
-                <input className="w3-input w3-border" type="text" placeholder="Name" required name="Name" ref={nameRef}/>
-                <input className="w3-input w3-section w3-border" type="text" placeholder="Email" required name="Email" ref={emailRef}/>
-                <input className="w3-input w3-section w3-border" type="text" placeholder="Subject" required name="Subject" ref={subjectRef}/>
-                <input className="w3-input w3-section w3-border" type="text" placeholder="Comment" required name="Comment" ref={commentRef}/>
+                <input className="w3-input w3-border" type="text" placeholder="Name" required name="Name"/>
+                <input className="w3-input w3-section w3-border" type="text" placeholder="Email" required name="Email"/>
+                <input className="w3-input w3-section w3-border" type="text" placeholder="Subject" required name="Subject"/>
+                <input className="w3-input w3-section w3-border" type="text" placeholder="Comment" required name="Comment"/>
                 <button className="w3-button w3-black w3-section" type="submit">
                     <i className="fa fa-paper-plane"></i> SEND MESSAGE
                 </button>
